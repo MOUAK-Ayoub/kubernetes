cbcdc0066fc02f2954cac5e98c42afab36acfbb4
Utilisateur <ayoub.mouak.2015@gmail.com>
2024-04-14 19:27:58 +0200

    [REFACTOR] Spring_batch module deleted

--- k8s-installation/terraform/user_data/k8s_addson.sh
+++ k8s-installation/terraform/user_data/k8s_addson.sh
@@ -6,7 +6,6 @@ sudo -u ec2-user -i <<'EOF'
 curl -LO https://get.helm.sh/helm-v3.14.4-linux-amd64.tar.gz
 tar xzvf helm-v3.14.4-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
-helm repo add stable https://charts.helm.sh/stable
 EOF
 
 echo '***********************************************k8s_addson script end*****************************************'
--- spring_batch_eks/CI-CD/Docker/build_image.sh
+++
@@ -1,8 +0,0 @@
-docker build -t ayoubmouak/sprinbatch-eks:1.0.0-SNAPSHOT -f ./CI-CD/Dockerfile .
-
- docker run \
-   --name springbatch
-   -e SPRING_DATASOURCE_URL=jdbc:postgresql://172.31.17.190:5432/test \
-   -d -p 8082:8082 ayoubmouak/spring-batch-eks:1.0.0-SNAPSHOT
-
-
